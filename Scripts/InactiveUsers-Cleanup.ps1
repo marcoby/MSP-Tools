ac46f39818ef6740c300e63e66578936aef15940
marcoby <vjackson601@gmail.com>
2022-01-03 11:46:42 -0800

    uploading IT Glue Module and changes to scripts

--- Scripts/InactiveUsers-Cleanup.ps1
+++ Scripts/InactiveUsers-Cleanup.ps1
@@ -1,88 +1,90 @@
 #Disable AD user not logged in X number of days, move to separate OU.
 
-
-<#
-
-Written By allenage.com
-
-
-#>
-
 #define Ou domain name here Where users Exits
 $DomainDN = Get-ADDomain | Select-Object DistinguishedName -ExpandProperty DistinguishedName
 
-#setting description for disabled users adding date of disabled, previous group membership
-
-$Ticket =
+#Description for disabled users adding date of disabled and ticket#
 $date = Get-date -Format "MM.dd.yy"
-$description = "Disabled by TIG on $Date - Ticket #$Ticket"
+$description = "Disabled by TIG on $Date"
 
-
+#Create Log File for Objects Disabled
 $worksfolder="C:\windows\LTSvc\Reports\AD Cleanup"
 if ( !(test-path "$worksfolder")) {
     Write-Verbose "Folder [$($worksfolder)] does not exist, creating"
     new-item $worksFolder -type directory -Force 
 }
-
-#define days days the period which users did not logged and you want to disable
-$DaysInactive = 30
-$today = Get-date -Format "MM_dd_yy"
-$time = (Get-Date).Adddays(-($DaysInactive)) 
-
-#you can change the path where users will be exported.
-$exportedpath= "$worksfolder\inactiveusers.csv"
+$DisabledUsersFile = "$worksfolder\DisabledUsers.csv"
+$DisabledComputersFile = "$worksfolder\DisabledComputers.csv"
+#if ( !(test-path "$DisabledUsersFile")) {
+#    Write-Verbose "Folder [$($DisabledUsersFile)] does not exist, creating"
+#    new-item $DisabledUsersFile -type directory -Force 
+#}
+#if ( !(test-path "$DisabledComputersFile")) {
+#    Write-Verbose "Folder [$($DisabledComputersFile)] does not exist, creating"
+#    new-item $DisabledComputersFile -type directory -Force 
+#}
 
 #Define OU where you want disabled users to be moved.
 $TIGDisabled="TIG MANAGE" # Type OU name and pass as variable 
+$TIGDisabledUsersGroup="TIG-DisabledUsers"
+$TIGDisabledComputersGroup="TIG-DisabledComputers"
 $DomainDN = Get-ADDomain | Select-Object DistinguishedName -ExpandProperty DistinguishedName
     Write-Host -ForegroundColor Green "Checking if OU exist $DomainDN"
 $oucheck = [adsi]::Exists("LDAP://OU=$TIGDisabled,$DomainDN")
+$OUDN = "OU=$TIGDisabled,$DomainDN"
 if($oucheck -eq $true) {
-    $text = "The OU already exist in Active Directory"
+    $text = "The TIG MANAGE OU already exist in Active Directory"
     Write-Host -ForegroundColor red $text.ToUpper()
 }
 else {
-    Write-Host "$TIGDisabled does not exist"
+    Write-Host "$TIGDisabled does not exist.  Creating now."
     New-ADOrganizationalUnit -Name "$TIGDisabled" -Path "$DomainDN"
 }
 $disabledou="OU=$TIGDisabled,$DomainDN"
-
-# Do not modify anything below this
+Write-Host -ForegroundColor Green "Checking if $TIGDisabledUsersGroup exist $OUDN"
+$usersgroupcheck= Get-ADGroup -LDAPFilter "(SAMAccountName=$TIGDisabledUsersGroup)"
+if($null -eq $UsersGroupCheck) {
+    New-ADGroup -Name "TIG-DisabledUsers" -SamAccountName TIG-DisabledUsers -GroupCategory Security -GroupScope Global -Description "Members of this group have been disabled by TIG MANAGE" -Path $OUDN
+}
+else {
+    Write-Host "TIG-DisabledUsers already exists"
+}
+Write-Host -ForegroundColor Green "Checking if $TIGDisabledComputersGroup exist $OUDN"
+$computersgroupcheck= Get-ADGroup -LDAPFilter "(SAMAccountName=$TIGDisabledComputersGroup)"
+if($null -eq $ComputersGroupCheck) {
+    New-ADGroup -Name "TIG-DisabledComputers" -SamAccountName TIG-DisabledComputers -GroupCategory Security -GroupScope Global -Description "Members of this group have been disabled by TIG MANAGE" -Path $OUDN
+}
+else {
+    Write-Host "TIG-DisabledComputers already exists"
+}
+#Cleanup Actions
 Import-Module activedirectory
-
-$target = Search-ADAccount -AccountInactive -TimeSpan 90.00:00:00 <# -SearchBase $ou #>| Where-Object{$_.enabled -eq $true} | Select-Object  samaccountname,name
-$target | export-csv $exportedpath -nti
-
-Foreach($user in $target){
-Disable-ADAccount -Identity $user.samaccountname -Verbose
-}
-
-## RESET A Random Password
-$length = 25
-$nonAlphaChars = 5
-$pwd = [System.Web.Security.Membership]::GeneratePassword($length, $nonAlphaChars)
-Foreach($user in $target){
-Try{
-Set-ADAccountPassword -identity $user.samaccountname -NewPassword (ConvertTo-SecureString -AsPlainText $pwd -force)
-}
-Catch{
-write-warning "$_ "}
-}
-
-## move To disabled OU
-Foreach($user in $target){  
+Write-Host "Starting Inactive User Cleanup"
+#Users Inactive in the last 90 days
+$targetUsers = Search-ADAccount -AccountInactive -TimeSpan 90.00:00:00 -UsersOnly | Where-Object{$_.enabled -eq $true} | Select-Object  samaccountname,name
+Foreach($user in $targetUsers){  
      $UserDN  = (Get-ADUser -Identity $user.samaccountname).distinguishedName  
-     Move-ADObject  -Identity $UserDN  -TargetPath $disabledou}
-
-## Set Description
-Foreach($user in $target){
-    $grp=get-aduser $user.samaccountname -Properties memberof | Select-Object name, @{n=’MemberOf’; e= { ( $_.memberof | ForEach-Object { (Get-ADObject $_).Name }) -join “,” }}
-    Set-ADUser $user.samaccountname -Description "$description" 
+     Move-ADObject  -Identity $UserDN  -TargetPath $disabledou
+     Set-ADUser $user.samaccountname -Description "$description"
+     $UserGroups = (Get-ADPrincipalGroupMembership -Identity $user.SamAccountName | Select-Object Name -ExpandProperty name) -join ','
+     get-aduser $user.SamAccountName -properties memberof,samaccountname,givenname,surname,description | Select-Object samaccountname,givenname,surname, @{name="Groups";expression={$UserGroups}},description | export-csv "$DisabledUsersFile" -Delimiter ";" -NoTypeInformation -Encoding UTF8 -Append
+     #Get-ADGroup -Filter {name -notlike "$TIGDisabledUsersGroup"}  | Remove-ADGroupMember -Members $user.samaccountname -Confirm:$False
+     Add-ADGroupMember -Identity $TIGDisabledUsersGroup -Members $user.samaccountname}
+Foreach($user in $targetUsers){  
+     Disable-ADAccount -Identity $user.samaccountname -Verbose
 }
 
-# Remove From all the Groups
-Foreach($user in $target){
-    Get-ADGroup -Filter {name -notlike "*domain users*"}  | Remove-ADGroupMember -Members $user.samaccountname -Confirm:$False 
+Write-Host "Starting Inactive Computers Cleanup"
+#Computers Inactive in the last 90 days
+$targetComputers = Search-ADAccount -AccountInactive -TimeSpan 90.00:00:00 -ComputersOnly | Where-Object{$_.enabled -eq $true} | Select-Object  samaccountname,name
+Foreach($computer in $targetComputers){  
+         $ComputerDN  = (Get-ADComputer -Identity $computer.samaccountname).distinguishedName  
+         Move-ADObject  -Identity $ComputerDN  -TargetPath $disabledou
+         Set-ADComputer $computer.samaccountname -Description "$description"
+         $ComputerGroups = (Get-ADPrincipalGroupMembership -Identity $computer.samaccountname | Select-Object Name -ExpandProperty name) -join ','
+         get-adcomputer $computer.samaccountname -properties memberof,Name,DNSHostName,DistinguishedName,description | Select-Object Name,DNSHostName,DistinguishedName, @{name="Groups";expression={$ComputerGroups}},description | export-csv "$DisabledComputersFile" -Delimiter ";" -NoTypeInformation -Encoding UTF8 -Append
+         #Get-ADGroup -Filter {name -notlike "$TIGDisabledGroup"}  | Remove-ADGroupMember -Members $user.samaccountname -Confirm:$False
+         Add-ADGroupMember -Identity $TIGDisabledComputersGroup -Members $computer.samaccountname}
+Foreach($computer in $targetComputers){   
+         Disable-ADAccount -Identity $computer.samaccountname -Verbose
 }
-
-New-ADGroup -Name "TIG-DisabledUsers" -SamAccountName TIGDisabledUsers -GroupCategory Security -GroupScope Global -Description "Members of this group have been disabled by TIG MANAGE"
